7b3f5376d8ad6a705da75321801a487f6b06659d
Ivan <ivan@MacBook-Air-Ivan.local>
2026-04-16 15:51:14 +0200

    server: openapi scheme gen readme

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['dev' => true, 'test' => true],
 ];
