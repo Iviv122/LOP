24bb8de0b748feaad682923600d3176b6ce2acd4
Ivan <ivan@MacBook-Air-Ivan.local>
2026-04-22 12:44:41 +0200

    logout works

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,31 +1,41 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import LoginForm from '../components/auth/LoginForm/LoginForm';
-import { useAuth } from '../lib/providers/auth';
-
+import LoginForm from '../components/auth/LoginForm/LoginForm'
+import { useAuth } from '../lib/providers/auth'
 
 function RootLayout() {
+  const auth = useAuth()
 
-  const auth = useAuth();
   return (
-
     auth.isAuthenticated ?
       <>
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{' '}
-          <Link to='/about' className="[&.active]:font-bold">
-            About
-          </Link>
+        <div className="p-2 flex gap-2 justify-around">
+          <span className="flex-2">
+            <Link to="/" className="[&.active]:font-bold">
+              Home
+            </Link>{' '}
+            <Link to="/about" className="[&.active]:font-bold">
+              About
+            </Link>
+          </span>
+
+          <button
+            className="flex-1 font-bold"
+            onClick={() => auth.logout()}
+          >
+            Logout
+          </button>
         </div>
+
         <hr />
         <Outlet />
         <TanStackRouterDevtools />
       </>
       :
-      <LoginForm/>
+      <LoginForm />
   )
 }
 
-export const Route = createRootRoute({ component: RootLayout })
+export const Route = createRootRoute({
+  component: RootLayout,
+})
