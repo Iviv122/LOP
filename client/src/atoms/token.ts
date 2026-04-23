3b4f22d4ad46185844ba5e3c8e3dff7cb1e67617
Ivan <ivan@MacBook-Air-Ivan.local>
2026-04-23 15:55:38 +0200

    third great frontend rewrite

---
+++ client/src/atoms/token.ts
@@ -0,0 +1,3 @@
+import { atomWithStorage } from "jotai/utils";
+
+export const token = atomWithStorage('token',"test");
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,36 +1,10 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-// Import the generated route tree
-import { routeTree } from './routeTree.gen'
-import { AuthProvider } from './lib/providers/auth'
-
-// Create a new router instance
-const router = createRouter({ routeTree })
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-const queryClient = new QueryClient()
-
-// Render the app
-const rootElement = document.getElementById('root')!
-if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-      <AuthProvider>
-        <StrictMode>
-          <QueryClientProvider client={queryClient} >
-            <RouterProvider router={router} />
-          </QueryClientProvider>
-        </StrictMode>
-      </AuthProvider>
-
-  )
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
