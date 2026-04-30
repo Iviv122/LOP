8fd2046ba616db4fdfcb74548d28e33d7b81b29c
Ivan <ivan@MacBook-Air-Ivan.local>
2026-04-30 15:05:27 +0200

    TODO:: server: create test user

---
+++ server/src/Command/CreateTestUserCommand.php
@@ -0,0 +1,62 @@
+<?php
+// src/Command/CreateAdminCommand.php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-test-user',
+    description: 'Creates an initial admin user',
+)]
+class CreateTestUserCommand extends Command
+{
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+
+        $this->em = $em;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $username = 'test';
+        $plainPassword = 'test';
+
+        $existing = $this->em->getRepository(User::class)
+            ->findOneBy(['username' => $username]);
+
+        if ($existing) {
+            $io->warning('Admin user already exists.');
+            return Command::SUCCESS;
+        }
+
+        $user = new User();
+        $user->setUsername($username);
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success('Admin user created!');
+        $io->note("Username: $username");
+        $io->note("Password: $plainPassword");
+
+        return Command::SUCCESS;
+    }
+}
